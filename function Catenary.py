760292bf9971e3dfbac80389d3577d18577568df
Mila Chabassier <mili25cha@gmail.com>
2025-04-07 21:02:50 +0200

    fitting a catenary model

--- function Catenary.py
+++ function Catenary.py
@@ -5,13 +5,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from scipy.optimize import curve_fit
 
+#from sklearn.model_selection import train_test_split
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.metrics import accuracy_score
 
-def catenary(x0, y0, c, x):
-    return y0 + c * (math.cosh((x-x0)/c) - 1)
+def catenary(x, x0, z0, c):
+    return z0 + c * (np.cosh((x-x0)/c) - 1)
 
 df_easy = pd.read_parquet('LiDAR datasets/lidar_cable_points_easy.parquet') #1502 rows * 3 columns
 
@@ -29,24 +30,13 @@ Z = (Z - z_mean)/z_dev
 
 ## Differentiate the clusters
 
-# I try to find the normal vector of the three planes, assuming that they are parallel
-model = LinearRegression()
-points = np.column_stack((X,Y))
-model.fit(points, Z)
-
-a, b = model.coef_
-c = model.intercept_
-
-print('equation:', a, b,c)
-n = [a, -1] # normal vector of the 3 planes
-
 ## Rotating the plane
-theta = -3*math.pi/4
+theta = -3*math.pi/4    # arbitrarily chosen
 cos_theta = math.cos(theta)
 sin_theta = math.sin(theta)
 Xp = np.array(cos_theta*X + sin_theta * Y)
 
-# Clustering
+### Step 1: Clustering
 kmeans = KMeans(n_clusters = 3)
 labels = kmeans.fit_predict(Xp.reshape(-1,1))  # labels of each point
 centers = kmeans.cluster_centers_          # coordinate of each cluster
@@ -57,8 +47,67 @@ data1 = df[labels == 1]
 data2 = df[labels == 2]
 
 ## Display the datasets in 3D
-plt.figure()
+"""plt.figure()
 axes = plt.axes(projection="3d")
 axes.scatter(df['x'], df['y'], df['z'], c=labels)
 plt.title("Clustering on the easy dataset")
 plt.show()
+"""
+### Step 2: Defining the planes
+#First plane
+# Finding the best fitting plane => making a linear regression
+
+model = LinearRegression()
+points = np.column_stack((data0['x'], data0['y']))
+model.fit(points, data0['z'])
+
+a, b = model.coef_
+c = model.intercept_
+
+print(f'equation of the plane: {a}x + {b}y - z + {c} = 0')
+n = [a, b, -1] # normal vector of the plane
+
+
+### Step : Find the best fitting curve
+#Let us work in the right plane
+X,Y, Z = data0['x'], data0['y'], data0['z']
+
+r = (a**2 + b**2)**0.5
+sin_theta = a/r
+cos_theta = b/r
+
+Xp = np.array(cos_theta*X + sin_theta * Y)
+
+z0_init = data0['z'].min()
+x0_init = data0[data0['z'] == z0_init]['x'].iloc[0]
+print(x0_init)
+
+y0_init = data0[data0['z'] == z0_init]['y'].iloc[0]
+print(y0_init)
+x0_init = cos_theta*x0_init + sin_theta * y0_init
+init = [x0_init, y0_init, z0_init]
+print("initial", init)
+
+#Fit the best curve
+popt, pcov = curve_fit(catenary, Xp, Z, p0 = [x0_init, z0_init, 1])
+x0, z0, c = popt
+
+print(f"Best parameters : c = {c}, x0 = {x0}, z0 = {z0}")
+"""plt.scatter(Xp, Z, label='Données observées', color='red')
+plt.plot(Xp, catenary(Xp, *popt), label='Modèle ajusté', color='blue', linestyle='None', marker = '+')
+plt.legend()
+plt.show()
+"""
+# Rotating inverted
+x0p = cos_theta*x0 - sin_theta * z0
+popt = x0p, z0, c
+print(f"Best parameters : c = {c}, x0 = {x0p}, z0 = {z0}")
+
+plt.figure()
+axes = plt.axes(projection="3d")
+plt.scatter(X, Y, Z, label='Dataset', color='red')
+plt.plot(X, Y, catenary(X, *popt), label='Catenary model', color='blue', linestyle='None', marker = '+')
+
+plt.legend()
+plt.show()
+
